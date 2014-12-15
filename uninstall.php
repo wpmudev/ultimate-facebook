f3c179dd809564f189cf11c3e22ac7c306a57e02
Umesh Kumar <umeshsingla05@gmail.com>
2014-12-15 18:40:34 +0530

    remove debug code

---
+++ uninstall.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Remove plugin settings data
+ *
+ * @since 2.7.9
+ *
+ */
+
+//if uninstall not called from WordPress exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+global $wpdb;
+//Network_options
+$wdfb_option_keys = array(
+	'wdfb_api',
+	'wdfb_autopost',
+	'wdfb_widget_pack',
+	'wdfb_opengraph',
+	'wdfb_connect',
+	'widget_wdfb_widgetalbums',
+	'wdfb_button',
+	'wdfb_groups',
+	'wdfb_error_log',
+	'wdfb_notice_log'
+);
+//Delete options
+foreach ( $wdfb_option_keys as $key ) {
+	if ( is_multisite() ) {
+		global $wpdb;
+		$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
+		if ( $blogs ) {
+			foreach ( $blogs as $blog ) {
+				switch_to_blog( $blog['blog_id'] );
+				delete_option( $key );
+			}
+			restore_current_blog();
+		}
+	} else {
+		delete_site_option( $key );
+	}
+}
+//Remove Cron Job
+wp_clear_scheduled_hook( 'wdfb_import_comments' );
+?>
