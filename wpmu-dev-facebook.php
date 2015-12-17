63a79a1bd329451868d61aaf47613ca792651f04
JulienZ <julien@incsub.com>
2015-12-17 14:19:58 +0100

    Changelog and version updated

--- wpmu-dev-facebook.php
+++ wpmu-dev-facebook.php
@@ -3,7 +3,7 @@
 Plugin Name: Ultimate Facebook
 Plugin URI: http://premium.wpmudev.org/project/ultimate-facebook
 Description: Easy Facebook integration: share your blog posts, autopost to your wall, login and registration integration, BuddyPress profiles support and more. Please, configure the plugin first.
-Version: 2.8.1.1
+Version: 2.8.1.2
 Text Domain: wdfb
 Author: WPMU DEV
 Author URI: http://premium.wpmudev.org
